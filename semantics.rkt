0e4a12c12db8dbe51558c6b4cbb8c763602c566a
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-11-01 16:46:02 +0100

    Implemented and tested semantics.

--- semantics.rkt
+++ semantics.rkt
@@ -9,5 +9,18 @@
 
 ; your implemenation of the semantics
 
-; implement (push-stack val a-state), (pop-stack a-state) and (dump-stack a-state)
+; (push-stack val a-state) returns void
+( define (push-stack val a-state) (set-state-stack! a-state (cons val (state-stack a-state))) )
+; (pop-stack a-state) returns the popped valued
+( define (pop-stack      a-state)
+   (let ([rtrn (car (state-stack a-state))])
+     (set-state-stack! a-state (cdr (state-stack a-state)))  ; nicer way of doing this?
+     rtrn
+   )
+)
+; (dump-stack a-state)
+( define (dump-stack     a-state) (write (state-stack a-state)) )
+
 ; implement (get-word name a-state) and (set-word name body a-state)
+( define (get-word name a-state)      (hash-ref  (state-words a-state) name) )
+( define (set-word name body a-state) (hash-set! (state-words a-state) name body) )
